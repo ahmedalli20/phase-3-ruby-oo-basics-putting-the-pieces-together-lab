db380f4fe059536026f22111bca9199d61052026
ahmedalli20 <ahmed.alistudent.moringaschool.com>
2022-10-19 13:04:39 +0300

    completed

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,19 @@
 
+class Book
+    attr_accessor :title, :author, :genre, :page_count
+    
+    def initialize title
+       @title = title
+       @author = author
+       @page_count = page_count
+       @genre = genre
+       @turn_page = turn_page
+    end
+
+    def turn_page
+        puts ("Flipping the page...wow, you read fast!")
+    end 
+
+end
+
 
